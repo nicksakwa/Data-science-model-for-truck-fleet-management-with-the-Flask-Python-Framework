163a1e801673565f892c450083c8806cb3b77f9f
nsakwa <nsakwa@gmail.com>
2025-07-09 22:46:16 +0300

    repaid data dataframe

--- app.py
+++ app.py
@@ -16,3 +16,16 @@ data_finance={
 }
 df_finance=pd.DataFrame(data_finance)
 df_finance.loc[df_finance['Ownership Type']=='Owned',['Remaining balance', 'Buyout Price']]=0
+
+repair_data=[]
+for truck_id in truck_ids:
+    num_repairs= np.random.randint(0,10)
+    for _ in range(num repairs):
+        repair_data.append({
+            'Truck ID': truck_id,
+            'Date':pd.to_datetime('2024-01-01')+ pd.to_timedelta(np.random.randint(0,180), unit='D'),
+            'Total Cost':np.random.uniform(50,5000),
+            'Odometer Reading':np.random.randint(50000, 500000)
+        })
+    df_repairs= pd.DataFrame(repairs_data)
+    df_repairs['Date']= pd.to_datetime(df_repairs['Date'])
